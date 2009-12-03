996761cc8f8807f18db1f4713d75b812890e752f
Omprakash Dhyade c_odhyad@quicinc.com <c_odhyad@quicinc.com>
2009-12-03 12:06:45 -0800

    system/core: Change permissions of the msm_rotator device
    Change msm_rotator device permissions so that it could be
    opened by the Overlay HAL library.
    
    Change-Id: I94d52e083826ea220dabf50ae20136b91c510630

--- init/devices.c
+++ init/devices.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2007 The Android Open Source Project
+ * Copyright (c) 2009, Code Aurora Forum. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -118,6 +119,7 @@ static struct perms_ devperms[] = {
     { "/dev/tty0",          0660,   AID_ROOT,       AID_SYSTEM,     0 },
     { "/dev/graphics/",     0660,   AID_ROOT,       AID_GRAPHICS,   1 },
     { "/dev/msm_hw3dm",     0660,   AID_SYSTEM,     AID_GRAPHICS,   0 },
+    { "/dev/msm_rotator",   0660,   AID_SYSTEM,     AID_GRAPHICS,   0 },
     { "/dev/input/",        0660,   AID_ROOT,       AID_INPUT,      1 },
     { "/dev/eac",           0660,   AID_ROOT,       AID_AUDIO,      0 },
     { "/dev/cam",           0660,   AID_ROOT,       AID_CAMERA,     0 },
