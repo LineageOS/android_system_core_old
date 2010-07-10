4e63592ce5e3661ad2df4bd07b2c033ec1b2a501
Steve Kondik <shade@chemlab.org>
2010-07-10 19:25:02 -0400

    Set ANDROID_PROPERTY_WORKSPACE in exec.

--- init/builtins.c
+++ init/builtins.c
@@ -185,14 +185,19 @@ int do_exec(int nargs, char **args)
     pid = fork();
     if (!pid)
     {
-        execvp(par[0],par);
-	exit(0);
+        char tmp[32];
+        int fd, sz;
+        get_property_workspace(&fd, &sz);
+        sprintf(tmp, "%d,%d", dup(fd), sz);
+        setenv("ANDROID_PROPERTY_WORKSPACE", tmp, 1);
+        execve(par[0],par,environ);
+        exit(0);
     }
     else
     {
         waitpid(pid, &status, 0);
         if (WEXITSTATUS(status) != 0) {
-        	ERROR("exec: pid %1d exited with return code %d: %s", (int)pid, WEXITSTATUS(status), strerror(status));
+            ERROR("exec: pid %1d exited with return code %d: %s", (int)pid, WEXITSTATUS(status), strerror(status));
         }
         
     }
