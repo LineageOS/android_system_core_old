722c734dc8c5cafedf15d52de9c189e937b2540d
Koushik Dutta <koushd@gmail.com>
2010-08-02 11:23:47 -0700

    unpackbootimg (squashed)
    unpackbootimg ported forward from eclair
    
    Change-Id: I74d2df0b47d40e7105cc58c2b05f8f383dc7f8a0
    
    port forward pagesize arg from eclair
    
    Change-Id: Ia789a4f392a3890aa0efa7efb42032482b48beb0
    
    unpackbootimg should output BOARD_PAGE_SIZE
    
    Change-Id: Ieb5cda01943a33da97eee4d025f56c2c6e7560e8
    
    output page size in mkbootimg
    
    add an option to override the pagesize for boot images created by nubs. ie, acer liquid mkliquidbootimg.
    
    Change-Id: Ie0c7e67edf5ae59019517e72b9be8c0b81388e41

--- mkbootimg/mkbootimg.c
+++ mkbootimg/mkbootimg.c
@@ -65,6 +65,7 @@ int usage(void)
             "       [ --board <boardname> ]\n"
             "       [ --base <address> ]\n"
             "       [ --pagesize <pagesize> ]\n"
+            "       [ --ramdiskaddr <address> ]\n"
             "       -o|--output <filename>\n"
             );
     return 1;
