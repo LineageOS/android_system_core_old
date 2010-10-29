0edecb3209d6a71091118a4785ad2bf553e38ac1
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2010-10-30 00:01:22 +0100

    init: Add permissions for Capella CM3602 light sensor
    Change-Id: I393ceb9715ec1dbfcaf30801f9a828cb38abd15a

--- init/devices.c
+++ init/devices.c
@@ -146,6 +146,7 @@ static struct perms_ devperms[] = {
     { "/dev/cm3602",        0640,   AID_COMPASS,    AID_SYSTEM,     0 },
     { "/dev/akm8976_pffd",  0640,   AID_COMPASS,    AID_SYSTEM,     0 },
     { "/dev/lightsensor",   0640,   AID_SYSTEM,     AID_SYSTEM,     0 },
+    { "/dev/cm3602_alsps",  0640,   AID_SYSTEM,     AID_SYSTEM,     0 },
 #ifdef AUDIOV2
     { "/dev/msm_pcm_dec",   0666,   AID_SYSTEM,     AID_AUDIO,      1 },
     { "/dev/snd/controlC0", 0666,   AID_SYSTEM,     AID_AUDIO,      1 },
