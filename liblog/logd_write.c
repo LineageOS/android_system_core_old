a96bae8371dd2ecb95e6f49a47ddffb67b13e0f2
Steve Kondik <shade@chemlab.org>
2010-11-13 03:54:26 -0500

    liblog: I <3 you Kineto, but STFU.
    Kineto's native library obliterates the logs with debug info, which
    makes me a sad panda when trying to debug other things. Send it to the
    RADIO bucket instead.
    
    Change-Id: I6e6a7a54a84d41cd189f9eb48518b990b8b13a89

--- liblog/logd_write.c
+++ liblog/logd_write.c
@@ -146,7 +146,10 @@ int __android_log_write(int prio, const char *tag, const char *msg)
         !strcmp(tag, "STK") ||
         !strcmp(tag, "CDMA") ||
         !strcmp(tag, "PHONE") ||
-        !strcmp(tag, "SMS"))
+        !strcmp(tag, "SMS") ||
+        !strcmp(tag, "KINETO") ||
+        !strncmp(tag, "KIPC", 4) ||
+        !strncmp(tag, "Kineto", 6))
             log_id = LOG_ID_RADIO;
 
     vec[0].iov_base   = (unsigned char *) &prio;
@@ -174,7 +177,10 @@ int __android_log_buf_write(int bufID, int prio, const char *tag, const char *ms
         !strcmp(tag, "STK") ||
         !strcmp(tag, "CDMA") ||
         !strcmp(tag, "PHONE") ||
-        !strcmp(tag, "SMS"))
+        !strcmp(tag, "SMS") ||
+        !strcmp(tag, "KINETO") ||
+        !strncmp(tag, "KIPC", 4) ||
+        !strncmp(tag, "Kineto", 6))
             bufID = LOG_ID_RADIO;
 
     vec[0].iov_base   = (unsigned char *) &prio;
