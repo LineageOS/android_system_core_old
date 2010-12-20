8964d3defc48322b6dd64cec8c55d5d3c4431913
Naveen Kalla <nkalla@codeaurora.org>
2010-12-20 14:37:49 -0800

    Add group qcom_oncrpc and give permissions to ril-daemon and /dev/oncrpc
    Change-Id: I022aae4a81c720a026ea6c1a41bda30acbe60224
    
    diag: Remove world read/write permissions from /dev/diag
    
    /dev/diag has world read/write permissions which is considered
    a security risk.  Remove these permissions.  Add new qcom_diag
    group for access to /dev/diag
    
    CRs-Fixed: 356415
    (cherry picked from commit 14c85ec56fd47c5eb143617a3bd5a6e7ee39cb6e)
    
    Change-Id: I536323a97e19a07bbd8eadffa0265d1955590285

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -93,6 +93,8 @@
 #define AID_NET_BW_STATS  3006  /* read bandwidth statistics */
 #define AID_NET_BW_ACCT   3007  /* change bandwidth statistics accounting */
 #define AID_NET_BT_STACK  3008  /* bluetooth: access config files */
+#define AID_QCOM_ONCRPC   3009  /* can read/write /dev/oncrpc files */
+#define AID_QCOM_DIAG     3010  /* can read/write /dev/diag */
 
 #define AID_EVERYBODY     9997  /* shared between all apps in the same profile */
 #define AID_MISC          9998  /* access to misc storage */
@@ -149,6 +151,8 @@ static const struct android_id_info android_ids[] = {
     { "sdcard_r",      AID_SDCARD_R, },
     { "clat",          AID_CLAT, },
     { "loop_radio",    AID_LOOP_RADIO, },
+    { "qcom_oncrpc",   AID_QCOM_ONCRPC, },
+    { "qcom_diag",     AID_QCOM_DIAG, },
     { "mediadrm",      AID_MEDIA_DRM, },
     { "package_info",  AID_PACKAGE_INFO, },
     { "sdcard_pics",   AID_SDCARD_PICS, },
