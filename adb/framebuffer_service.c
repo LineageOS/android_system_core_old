8a9cd24a825c8f5533872b2c1ca964e561c06d46
FUKAUMI Naoki <naobsd@gmail.com>
2011-05-07 22:19:42 +0900

    cherry-pick latest change from nv-tegra.nvidia.com
    > adbd: unify frame buffer service support for both k32 and k36
    >
    > - use screencap to capture frame buffer for ddms
    >
    > bug 791664
    >
    > Change-Id: I0ab0f6991a74ea879e2d404b8323b9624bea4292
    > Reviewed-on: http://git-master/r/24058
    > Tested-by: Peter Zu <pzu@nvidia.com>
    > Reviewed-by: Daniel Willemsen <dwillemsen@nvidia.com>
    > Reviewed-by: Venkata (Muni) Anda <vanda@nvidia.com>
    
    Change-Id: I0cd89f785a49f7ce99080827174845208c5516bb

--- adb/framebuffer_service.c
+++ adb/framebuffer_service.c
@@ -19,13 +19,20 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
+#include <errno.h>
+#ifdef USE_SCREENCAP
+#include <hardware/hardware.h>
 
+#include "sysdeps.h"
+#endif
 #include "fdevent.h"
 #include "adb.h"
 
+#ifndef USE_SCREENCAP
 #include <linux/fb.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
+#endif
 
 /* TODO:
 ** - sync with vsync to avoid tearing
@@ -49,6 +56,101 @@ struct fbinfo {
     unsigned int alpha_length;
 } __attribute__((packed));
 
+#ifdef USE_SCREENCAP
+static int fill_format(struct fbinfo* info, int format)
+{
+    // bpp, red, green, blue, alpha
+
+    static const int format_map[][9] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},   // INVALID
+        {32, 0, 8, 8, 8, 16, 8, 24, 8}, // HAL_PIXEL_FORMAT_RGBA_8888
+        {32, 0, 8, 8, 8, 16, 8, 0, 0}, // HAL_PIXEL_FORMAT_RGBX_8888
+        {24, 16, 8, 8, 8, 0, 8, 0, 0},  // HAL_PIXEL_FORMAT_RGB_888
+        {16, 11, 5, 5, 6, 0, 5, 0, 0},  // HAL_PIXEL_FORMAT_RGB_565
+        {32, 16, 8, 8, 8, 0, 8, 24, 8}, // HAL_PIXEL_FORMAT_BGRA_8888
+        {16, 11, 5, 6, 5, 1, 5, 0, 1},  // HAL_PIXEL_FORMAT_RGBA_5551
+        {16, 12, 4, 8, 4, 4, 4, 0, 4}   // HAL_PIXEL_FORMAT_RGBA_4444
+    };
+    const int *p;
+
+    if (format == 0 || format > HAL_PIXEL_FORMAT_RGBA_4444)
+        return -ENOTSUP;
+
+    p = format_map[format];
+
+    info->bpp = *(p++);
+    info->red_offset = *(p++);
+    info->red_length = *(p++);
+    info->green_offset = *(p++);
+    info->green_length = *(p++);
+    info->blue_offset = *(p++);
+    info->blue_length = *(p++);
+    info->alpha_offset = *(p++);
+    info->alpha_length = *(p++);
+
+    return 0;
+}
+
+void framebuffer_service(int fd, void *cookie)
+{
+    char x[512];
+
+    struct fbinfo fbinfo;
+    int w, h, f;
+    int s[2];
+    int fb;
+    unsigned int i;
+    int rv;
+
+    if(adb_socketpair(s)) {
+        printf("cannot create service socket pair\n");
+        goto done;
+    }
+
+    pid_t pid = fork();
+    if (pid < 0) {
+        printf("- fork failed: %s -\n", strerror(errno));
+        goto done;
+    }
+
+    if (pid == 0) {
+        dup2(s[1], STDOUT_FILENO);
+        const char* cmd_path = "/system/bin/screencap";
+        const char* cmd = "screencap";
+        execl(cmd_path, cmd, NULL);
+        exit(1);
+    }
+
+    fb = s[0];
+
+    /* read w, h, f */
+    if (readx(fb, &w, 4)) goto done;
+    if (readx(fb, &h, 4)) goto done;
+    if (readx(fb, &f, 4)) goto done;
+
+    fbinfo.version = DDMS_RAWIMAGE_VERSION;
+    fbinfo.size = w * h * 4;
+    fbinfo.width = w;
+    fbinfo.height = h;
+    rv = fill_format(&fbinfo, f);
+    if (rv != 0) goto done;
+
+    if(writex(fd, &fbinfo, sizeof(fbinfo))) goto done;
+
+    for(i = 0; i < fbinfo.size; i += sizeof(x)) {
+        if(readx(fb, &x, sizeof(x))) goto done;
+        if(writex(fd, &x, sizeof(x))) goto done;
+    }
+
+    if(readx(fb, &x, fbinfo.size % sizeof(x))) goto done;
+    if(writex(fd, &x, fbinfo.size % sizeof(x))) goto done;
+
+done:
+    adb_close(s[0]);
+    adb_close(s[1]);
+    adb_close(fd);
+}
+#else
 void framebuffer_service(int fd, void *cookie)
 {
     struct fb_var_screeninfo vinfo;
@@ -104,3 +206,4 @@ done:
     if(fb >= 0) close(fb);
     close(fd);
 }
+#endif
