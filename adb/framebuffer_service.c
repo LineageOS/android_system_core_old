c32b12efb89b10f5d2cb88b463309dd982309993
FUKAUMI Naoki <naobsd@gmail.com>
2011-05-08 03:18:03 +0900

    restore old framebuffer_service() and use it as default new framebuffer_service is enabled by BOARD_USE_SCREENCAP
    Change-Id: I89eb1f5f1821e592372ff0873136b6382a6e5730

--- adb/framebuffer_service.c
+++ adb/framebuffer_service.c
@@ -19,12 +19,20 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
+#ifdef USE_SCREENCAP
 #include <errno.h>
 #include <hardware/hardware.h>
 
 #include "sysdeps.h"
+#endif
 #include "fdevent.h"
 #include "adb.h"
+
+#ifndef USE_SCREENCAP
+#include <linux/fb.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#endif
 
 /* TODO:
 ** - sync with vsync to avoid tearing
@@ -48,6 +56,7 @@ struct fbinfo {
     unsigned int alpha_length;
 } __attribute__((packed));
 
+#ifdef USE_SCREENCAP
 static int fill_format(struct fbinfo* info, int format)
 {
     // bpp, red, green, blue, alpha
@@ -141,3 +150,60 @@ done:
     adb_close(s[1]);
     adb_close(fd);
 }
+#else
+void framebuffer_service(int fd, void *cookie)
+{
+    struct fb_var_screeninfo vinfo;
+    int fb, offset;
+    char x[256];
+
+    struct fbinfo fbinfo;
+    unsigned i, bytespp;
+
+    fb = open("/dev/graphics/fb0", O_RDONLY);
+    if(fb < 0) goto done;
+
+    if(ioctl(fb, FBIOGET_VSCREENINFO, &vinfo) < 0) goto done;
+    fcntl(fb, F_SETFD, FD_CLOEXEC);
+
+    bytespp = vinfo.bits_per_pixel / 8;
+
+    fbinfo.version = DDMS_RAWIMAGE_VERSION;
+    fbinfo.bpp = vinfo.bits_per_pixel;
+    fbinfo.size = vinfo.xres * vinfo.yres * bytespp;
+    fbinfo.width = vinfo.xres;
+    fbinfo.height = vinfo.yres;
+    fbinfo.red_offset = vinfo.red.offset;
+    fbinfo.red_length = vinfo.red.length;
+    fbinfo.green_offset = vinfo.green.offset;
+    fbinfo.green_length = vinfo.green.length;
+    fbinfo.blue_offset = vinfo.blue.offset;
+    fbinfo.blue_length = vinfo.blue.length;
+    fbinfo.alpha_offset = vinfo.transp.offset;
+    fbinfo.alpha_length = vinfo.transp.length;
+
+    /* HACK: for several of our 3d cores a specific alignment
+     * is required so the start of the fb may not be an integer number of lines
+     * from the base.  As a result we are storing the additional offset in
+     * xoffset. This is not the correct usage for xoffset, it should be added
+     * to each line, not just once at the beginning */
+    offset = vinfo.xoffset * bytespp;
+
+    offset += vinfo.xres * vinfo.yoffset * bytespp;
+
+    if(writex(fd, &fbinfo, sizeof(fbinfo))) goto done;
+
+    lseek(fb, offset, SEEK_SET);
+    for(i = 0; i < fbinfo.size; i += 256) {
+      if(readx(fb, &x, 256)) goto done;
+      if(writex(fd, &x, 256)) goto done;
+    }
+
+    if(readx(fb, &x, fbinfo.size % 256)) goto done;
+    if(writex(fd, &x, fbinfo.size % 256)) goto done;
+
+done:
+    if(fb >= 0) close(fb);
+    close(fd);
+}
+#endif
