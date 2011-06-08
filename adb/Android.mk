c2b6baef623ffe7b8c26aff21f752cf9e5b727b8
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-06-08 04:08:51 +0200

    libncurses not required to build adb, and libncurses5 is no more compatible on debian testing

--- adb/Android.mk
+++ adb/Android.mk
@@ -16,7 +16,7 @@ EXTRA_SRCS :=
 ifeq ($(HOST_OS),linux)
   USB_SRCS := usb_linux.c
   EXTRA_SRCS := get_my_path_linux.c
-  LOCAL_LDLIBS += -lrt -lncurses -lpthread
+  LOCAL_LDLIBS += -lrt -lpthread
 endif
 
 ifeq ($(HOST_OS),darwin)
