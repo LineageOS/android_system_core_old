c5586a7155f19795d9671fbf70cd548ea7583682
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-12-19 03:53:49 +0100

    reduce logs: no info on normal process exit, and basename as tag
    Change-Id: Ife72729eaee2a366bd4226f3fbe3ba074219c974

--- logwrapper/Android.mk
+++ logwrapper/Android.mk
@@ -5,3 +5,14 @@ LOCAL_SRC_FILES:= logwrapper.c
 LOCAL_MODULE := logwrapper
 LOCAL_STATIC_LIBRARIES := liblog
 include $(BUILD_EXECUTABLE)
+
+
+ifeq ($(BOARD_USES_BOOTMENU),true)
+include $(CLEAR_VARS)
+LOCAL_MODULE := logwrapper.bin
+LOCAL_MODULE_TAGS := eng debug
+LOCAL_SRC_FILES:= logwrapper.c
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_STATIC_LIBRARIES := libc libcutils liblog
+include $(BUILD_EXECUTABLE)
+endif
