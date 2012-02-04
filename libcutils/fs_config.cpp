3a452c3dc4928babfb560cb5c792e2bcf04f2ec2
Michaël Burtin <bukington@gmail.com>
2012-02-05 00:55:17 +0100

    set /system/etc/init.d/* permissions
    ported from CM7
    
    Change-Id: I279d78679f7d779d6cd6bbc6c834d94706937ae8
    
    Former-commit-id: c8f575dda87bddbb2af4707d8ac323928bd51cd7

--- libcutils/fs_config.cpp
+++ libcutils/fs_config.cpp
@@ -199,6 +199,7 @@ static const struct fs_path_config android_files[] = {
     { 00755, AID_ROOT,      AID_SHELL,     0, "product/bin/*" },
     { 00750, AID_ROOT,      AID_SHELL,     0, "sbin/*" },
     { 00755, AID_ROOT,      AID_SHELL,     0, "system/bin/*" },
+    { 00755, AID_ROOT,      AID_SHELL,     0, "system/etc/init.d/*" },
     { 00755, AID_ROOT,      AID_ROOT,      0, "system/lib/valgrind/*" },
     { 00755, AID_ROOT,      AID_ROOT,      0, "system/lib64/valgrind/*" },
     { 00755, AID_ROOT,      AID_SHELL,     0, "system/xbin/*" },
