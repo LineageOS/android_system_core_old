6934c8ff849141d36790c663992bfbce5d7202cd
Dixon Peterson <dixonp@codeaurora.org>
2012-06-29 17:19:17 -0700

    diag: Remove world read/write permissions from /dev/diag
    /dev/diag has world read/write permissions which is considered
    a security risk.  Remove these permissions.  Add new qcom_diag
    group for access to /dev/diag
    
    CRs-Fixed: 356415
    (cherry picked from commit 14c85ec56fd47c5eb143617a3bd5a6e7ee39cb6e)
    
    Change-Id: I536323a97e19a07bbd8eadffa0265d1955590285

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -77,6 +77,7 @@
 #define AID_NET_BW_STATS  3006  /* read bandwidth statistics */
 #define AID_NET_BW_ACCT   3007  /* change bandwidth statistics accounting */
 #define AID_QCOM_ONCRPC   3008  /* can read/write /dev/oncrpc files */
+#define AID_QCOM_DIAG     3009  /* can read/write /dev/diag */
 
 #if defined(MOTOROLA_UIDS)
 #define AID_MOT_OSH       5000  /* OSH */
@@ -150,6 +151,7 @@ static const struct android_id_info android_ids[] = {
     { "net_bw_stats", AID_NET_BW_STATS, },
     { "net_bw_acct", AID_NET_BW_ACCT, },
     { "qcom_oncrpc", AID_QCOM_ONCRPC, },
+    { "qcom_diag", AID_QCOM_DIAG, },
 #if defined(MOTOROLA_UIDS)
     { "mot_osh",   AID_MOT_OSH, },
     { "mot_accy",  AID_MOT_ACCY, },
