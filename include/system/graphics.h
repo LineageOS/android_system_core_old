50de86cf3d5e7336b9d5059a54d90c69a1d25436
ST-Ericsson <void@stericsson.com>
2012-07-12 20:49:43 +0200

    Adding required ST-Ericsson changes to graphics.h
    Pixel formats have been added to graphics.h (they used to be in
    hardware.h). One is ST-Ericsson specific and the rest are just
    added since they were there in GB.
    
    Change-Id: Ib180a29a1fb0a57bae478dc9a3ba6607fa8cab23
    Signed-off-by: Patrik Ryd <patrik.ryd@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/70894
    Signed-off-by: Alin Jerpelea <jerpelea@gmail.com>

--- include/system/graphics.h
+++ include/system/graphics.h
@@ -109,10 +109,29 @@ enum {
      */
     HAL_PIXEL_FORMAT_RAW_SENSOR = 0x20,
 
+#ifdef STE_HARDWARE
+    /* STE: Added Support of YUV42XMBN, required for Copybit CC acceleration */
+    HAL_PIXEL_FORMAT_YCBCR42XMBN = 0xE,
+#endif
     /* Legacy formats (deprecated), used by ImageFormat.java */
     HAL_PIXEL_FORMAT_YCbCr_422_SP       = 0x10, // NV16
     HAL_PIXEL_FORMAT_YCrCb_420_SP       = 0x11, // NV21
+#ifdef STE_HARDWARE
+    HAL_PIXEL_FORMAT_YCbCr_422_P        = 0x12,
+    HAL_PIXEL_FORMAT_YCbCr_420_P        = 0x13,
+#endif
     HAL_PIXEL_FORMAT_YCbCr_422_I        = 0x14, // YUY2
+#ifdef STE_HARDWARE
+    HAL_PIXEL_FORMAT_YCbCr_420_I        = 0x15,
+    HAL_PIXEL_FORMAT_CbYCrY_422_I       = 0x16,
+    HAL_PIXEL_FORMAT_CbYCrY_420_I       = 0x17,
+    HAL_PIXEL_FORMAT_YCbCr_420_SP_TILED = 0x20,
+    HAL_PIXEL_FORMAT_YCbCr_420_SP       = 0x21,
+    HAL_PIXEL_FORMAT_YCrCb_420_SP_TILED = 0x22,
+    HAL_PIXEL_FORMAT_YCrCb_422_SP       = 0x23,
+    HAL_PIXEL_FORMAT_YCrCb_422_P        = 0x24,
+    HAL_PIXEL_FORMAT_YCrCb_420_P        = 0x25,
+#endif
 };
 
 
