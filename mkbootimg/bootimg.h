ffc01b50f7d44000a8ef0d116302cb0274588d34
David Ng <dave@codeaurora.org>
2012-07-27 17:15:03 -0700

    mkbootimg: Add --dt parameter to specify DT image
    New optional --dt parameter to specify a kernel device
    tree image.
    
    Change-Id: Ie29a11cbf4138426bfd19ae486d69a5fcbd8f442

--- mkbootimg/bootimg.h
+++ mkbootimg/bootimg.h
@@ -43,6 +43,7 @@ struct boot_img_hdr
 
     uint32_t tags_addr;    /* physical addr for kernel tags */
     uint32_t page_size;    /* flash page size we assume */
+    uint32_t dt_size;      /* device tree in bytes */
     uint32_t unused[2];    /* future expansion: should be 0 */
 
     uint8_t name[BOOT_NAME_SIZE]; /* asciiz product name */
@@ -66,10 +67,13 @@ struct boot_img_hdr
 ** +-----------------+
 ** | second stage    | o pages
 ** +-----------------+
+** | device tree     | p pages
+** +-----------------+
 **
 ** n = (kernel_size + page_size - 1) / page_size
 ** m = (ramdisk_size + page_size - 1) / page_size
 ** o = (second_size + page_size - 1) / page_size
+** p = (dt_size + page_size - 1) / page_size
 **
 ** 0. all entities are page_size aligned in flash
 ** 1. kernel and ramdisk are required (size != 0)
