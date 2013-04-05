01996abeda1240e04a86a0393e0b553dd6310412
Stephen Smalley <sds@tycho.nsa.gov>
2013-04-05 10:28:30 -0400

    Sync with master auditd.

--- auditd/Android.mk
+++ auditd/Android.mk
@@ -3,9 +3,13 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+# Override this in the BoardConfig.mk
+# to change the default size
+# Note: The value is in Kilobytes
+AUDITD_MAX_LOG_FILE_SIZEKB ?= 100
+
 LOCAL_SRC_FILES:= \
 	auditd.c \
-	netlink.c \
 	libaudit.c \
 	audit_log.c
 
@@ -13,7 +17,7 @@ LOCAL_SHARED_LIBRARIES := \
 	libcutils \
 	libc
 
-LOCAL_MODULE_TAGS:=optional
-LOCAL_MODULE:=auditd
-
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := auditd
+LOCAL_CFLAGS := -DAUDITD_MAX_LOG_FILE_SIZEKB=$(AUDITD_MAX_LOG_FILE_SIZEKB)
 include $(BUILD_EXECUTABLE)
--- auditd/auditd.c
+++ auditd/auditd.c
@@ -16,6 +16,8 @@
  * Written by William Roberts <w.roberts@sta.samsung.com>
  */
 
+#define LOG_TAG "auditd"
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -28,17 +30,20 @@
 #include <syslog.h>
 #include <unistd.h>
 
+#include <sys/capability.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#include <cutils/log.h>
 #include <cutils/klog.h>
+
+#include <cutils/log.h>
 #include <private/android_filesystem_config.h>
 
 #include <linux/capability.h>
 #include <linux/prctl.h>
 
-#include "auditd.h"
 #include "libaudit.h"
 #include "audit_log.h"
 
@@ -48,168 +53,172 @@
  *   1. Add a socket interface for sending events
  */
 
-#define LOG_DIR "/data/misc/audit"
-#define LOG_FILE LOG_DIR "/audit.log"
-#define OLD_LOG_FILE LOG_DIR "/audit.old"
+#ifndef AUDITD_MAX_LOG_FILE_SIZEKB
+#error "AUDITD_MAX_LOG_FILE_SIZEKB not defined by makefile!"
+#endif
 
-#define MAX_LOG_FILE_SIZE (1024 * 100)
+#define AUDITD_LOG_DIR "/data/misc/audit"
+#define AUDITD_LOG_FILE AUDITD_LOG_DIR "/audit.log"
+#define AUDITD_OLD_LOG_FILE AUDITD_LOG_DIR "/audit.old"
+
+#define AUDITD_MAX_LOG_FILE_SIZE (1024 * AUDITD_MAX_LOG_FILE_SIZEKB)
 
 static volatile int quit = 0;
 
-static void signal_handler(int sig) {
-	switch (sig) {
-		case SIGINT:
-			quit = 1;
-		break;
-	}
-	return;
+static void signal_handler(int sig)
+{
+    switch (sig) {
+    case SIGINT:
+    case SIGTERM:
+        quit = 1;
+        break;
+    }
+    return;
 }
 
-static void usage(char *cmd) {
-	printf("%s - log audit events from the kernel\n"
-			"OPTIONS\n"
-			"-k - search dmesg on startup for audit events\n"
-			"\n",
-			cmd);
+static void usage(char *cmd)
+{
+    printf("%s - log audit events from the kernel\n"
+            "OPTIONS\n"
+            "-k - search dmesg on startup for audit events\n"
+            "\n", cmd);
 }
 
 #define RAISE(ary, c) ary[CAP_TO_INDEX(c)].permitted |= CAP_TO_MASK(c);
 
-static void drop_privileges_or_die(void) {
-	struct __user_cap_header_struct capheader;
-	struct __user_cap_data_struct capdata[2];
+static void drop_privileges_or_die(void)
+{
 
-	if (prctl(PR_SET_KEEPCAPS, 1) < 0) {
-		ERROR("Failed on prctl KEEPCAPS: %s\n", strerror(errno));
-		exit(1);
-	}
+    struct __user_cap_header_struct capheader;
+    struct __user_cap_data_struct capdata[2];
 
-	if (setgid(AID_AUDIT) < 0) {
-		ERROR("Failed on setgid: %s\n", strerror(errno));
-		exit(1);
-	}
+    if (prctl(PR_SET_KEEPCAPS, 1) < 0) {
+        SLOGE("Failed on prctl KEEPCAPS: %s", strerror(errno));
+        exit(1);
+    }
 
-	if (setuid(AID_AUDIT) < 0) {
-		ERROR("Failed on setuid: %s\n", strerror(errno));
-		exit(1);
-	}
+    if (setgid(AID_AUDIT) < 0) {
+        SLOGE("Failed on setgid: %s", strerror(errno));
+        exit(1);
+    }
 
-	memset(&capheader, 0, sizeof(capheader));
-	memset(&capdata, 0, sizeof(capdata));
-	capheader.version = _LINUX_CAPABILITY_VERSION_3;
-	capheader.pid = 0;
+    if (setuid(AID_AUDIT) < 0) {
+        SLOGE("Failed on setuid: %s", strerror(errno));
+        exit(1);
+    }
 
-	RAISE(capdata, CAP_AUDIT_CONTROL);
-	RAISE(capdata, CAP_SYSLOG);
+    memset(&capheader, 0, sizeof(capheader));
+    memset(&capdata, 0, sizeof(capdata));
+    capheader.version = _LINUX_CAPABILITY_VERSION_3;
+    capheader.pid = 0;
 
-	capdata[0].effective = capdata[0].permitted;
-	capdata[1].effective = capdata[1].permitted;
-	capdata[0].inheritable = 0;
-	capdata[1].inheritable = 0;
+    RAISE(capdata, CAP_AUDIT_CONTROL);
+    RAISE(capdata, CAP_SYSLOG);
 
-	if (capset(&capheader, &capdata[0]) < 0) {
-		ERROR("Failed on capset: %s\n", strerror(errno));
-		exit(1);
-	}
+    capdata[0].effective = capdata[0].permitted;
+    capdata[1].effective = capdata[1].permitted;
+    capdata[0].inheritable = 0;
+    capdata[1].inheritable = 0;
+
+    if (capset(&capheader, &capdata[0]) < 0) {
+        SLOGE("Failed on capset: %s", strerror(errno));
+        exit(1);
+    }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    int c;
+    int rc;
+    int audit_fd = -1;
+    int check_kernel_log = 0;
 
-	int c;
+    struct pollfd pfds;
+    struct audit_reply rep;
+    struct sigaction action;
+    audit_log *l = NULL;
 
-	struct audit_reply rep;
-	struct sigaction action;
+    SLOGI("Starting up");
 
-	int rc;
-	int audit_fd = -1;
-	int check_kernel_log = 0;
+    drop_privileges_or_die();
 
-	audit_log *l = NULL;
-	ssize_t total_bytes = 0;
-	struct pollfd pfds;
+    /* register the signal handler */
+    action.sa_handler = signal_handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+    rc = sigaction(SIGINT, &action, NULL);
+    if (rc < 0) {
+        rc = errno;
+        SLOGE("Failed on set signal handler: %s", strerror(errno));
+        goto err;
+    }
 
-	INFO("auditd:  starting up\n");
+    while ((c = getopt(argc, argv, "k")) != -1) {
+        switch (c) {
+        case 'k':
+            check_kernel_log = 1;
+            break;
+        default:
+            usage(argv[0]);
+            goto err;
+        }
+    }
 
-	drop_privileges_or_die();
+    /* Open the netlink socket for audit events */
+    audit_fd = audit_open();
+    if (audit_fd < 0) {
+        rc = errno;
+        SLOGE("Failed on audit_set_pid with error: %s", strerror(errno));
+        goto err;
+    }
 
-	/* register the signal handler */
-	action.sa_handler = signal_handler;
-	sigemptyset (&action.sa_mask);
-	action.sa_flags = 0;
-	rc = sigaction (SIGINT, &action, NULL);
-	if (rc < 0) {
-		rc = errno;
-		ERROR("Failed on set signal handler: %s\n", strerror(errno));
-		goto err;
-	}
+    l = audit_log_open(AUDITD_LOG_FILE, AUDITD_OLD_LOG_FILE, AUDITD_MAX_LOG_FILE_SIZE);
+    if (!l) {
+        SLOGE("Failed on audit_log_open");
+        goto err;
+    }
 
-	while((c = getopt(argc, argv, "k")) != -1) {
-		switch(c) {
-		case 'k':
-			check_kernel_log = 1;
-			break;
-		default:
-			usage(argv[0]);
-			goto err;
-		}
-	}
+    if (audit_set_pid(audit_fd, getpid(), WAIT_YES) < 0) {
+        rc = errno;
+        SLOGE("Failed on audit_set_pid with error: %s", strerror(errno));
+        goto err;
+    }
 
-	/* Open the netlink socket for audit events */
-	audit_fd = audit_open();
-	if (audit_fd < 0) {
-		rc = errno;
-		ERROR("Failed on audit_set_pid with error: %s\n", strerror(errno));
-		goto err;
-	}
+    pfds.fd = audit_fd;
+    pfds.events = POLLIN;
 
-	l = audit_log_open(LOG_FILE, OLD_LOG_FILE, MAX_LOG_FILE_SIZE);
-	if(!l) {
-		ERROR("Failed on audit_log_open\n");
-		goto err;
-	}
+    if (check_kernel_log) {
+        audit_log_put_kmsg(l);
+    }
 
-	if (audit_set_pid(audit_fd, getpid(), WAIT_YES) < 0) {
-		rc = errno;
-		ERROR("Failed on audit_set_pid with error: %s\n", strerror(errno));
-		goto err;
-	}
+    while (!quit) {
 
-	pfds.fd = audit_fd;
-	pfds.events = POLLIN;
+        /* Start reading for events */
+        rc = poll(&pfds, 1, -1);
+        if (rc == 0) {
+            continue;
+        } else if (rc < 0) {
+            if (errno != EINTR) {
+                SLOGE("Failed to poll audit log socket: %d : %s", errno, strerror(errno));
+            }
+            continue;
+        }
 
-	if(check_kernel_log) {
-		audit_log_put_kmsg(l);
-	}
+        if (audit_get_reply(audit_fd, &rep, GET_REPLY_BLOCKING, 0) < 0) {
+            SLOGE("Failed on audit_get_reply with error: %s", strerror(errno));
+            continue;
+        }
 
-	while (!quit) {
-
-		/* Start reading for events */
-		rc = poll(&pfds, 1, -1);
-		if (rc == 0) {
-			continue;
-		}
-		else if(rc < 0) {
-			if (errno != EINTR) {
-				ERROR("Failed to poll audit log socket: %d : %s\n", errno, strerror(errno));
-			}
-			continue;
-		}
-
-		if (audit_get_reply(audit_fd, &rep, GET_REPLY_BLOCKING, 0) < 0) {
-			ERROR("Failed on audit_get_reply with error: %s\n", strerror(errno));
-			continue;
-		}
-
-		audit_log_write(l, &rep);
-		/* Keep reading for events */
-	}
+        audit_log_write(l, "type=%d msg=%.*s\n", rep.type, rep.len, rep.msg.data);
+        /* Keep reading for events */
+    }
 
 err:
-	INFO("auditd:  exiting\n");
-	if (audit_fd >= 0) {
-		audit_set_pid(audit_fd, 0, WAIT_NO);
-		audit_close(audit_fd);
-	}
-	audit_log_close(l);
-	return rc;
+    SLOGI("Exiting");
+    if (audit_fd >= 0) {
+        audit_set_pid(audit_fd, 0, WAIT_NO);
+        audit_close(audit_fd);
+    }
+    audit_log_close(l);
+    return rc;
 }
