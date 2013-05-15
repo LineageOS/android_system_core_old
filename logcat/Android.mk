bb6ef12b8dfebc182224742cfd617b45a5cf33d0
Chirayu Desai <cdesai@cyanogenmod.org>
2013-05-15 12:45:51 +0530

    lolcat
    Change-Id: I4fecd80884a86c2d9bb720c9e821bd1605814d8c

--- logcat/Android.mk
+++ logcat/Android.mk
@@ -10,3 +10,13 @@ LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_MODULE:= logcat
 
 include $(BUILD_EXECUTABLE)
+
+SYMLINK := $(TARGET_OUT)/bin/lolcat
+$(SYMLINK): LOGCAT_BINARY := $(LOCAL_MODULE)
+$(SYMLINK): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
+	@echo "Symlink: $@ -> $(LOGCAT_BINARY)"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf $(LOGCAT_BINARY) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINK)
