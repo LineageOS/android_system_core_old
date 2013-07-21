5311bbd2a884fd14654b723dd9d4822023b549f7
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-07-21 14:25:47 +0100

    fs_mgr: Drop the context mount option from non-selinux builds
    Transitioning devices may have the context option present in the
    fstab, even if we're not doing selinux builds.
    Since we don't set or even create the contexts on those builds,
    this will make mount fail, and either the mountpoint ends up absent,
    or boot fails entirely
    
    Change-Id: Ic5cc7f5632cc3d62459dfaaf7719d662737e1641

--- fs_mgr/Android.mk
+++ fs_mgr/Android.mk
@@ -3,6 +3,10 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+ifeq ($(HAVE_SELINUX),true)
+LOCAL_CFLAGS += -DHAVE_SELINUX
+endif
+
 LOCAL_SRC_FILES:= fs_mgr.c
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
