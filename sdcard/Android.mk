580215d89d0a3c7cec75e15bcb8183092d122991
Steve Kondik <shade@chemlab.org>
2013-08-23 21:12:12 -0700

    sdcard: Allow building as a static library
     * Split main() out in order to allow linking with a multi-call
       executable such as recovery.
    
    Change-Id: I3325135e47e4dd5af3912f5ce67ec64007d54a63

--- sdcard/Android.mk
+++ sdcard/Android.mk
@@ -3,11 +3,19 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := sdcard.cpp fuse.cpp
-LOCAL_MODULE := sdcard
+LOCAL_MODULE := libsdcard
 LOCAL_CFLAGS := -Wall -Wno-unused-parameter -Werror
 LOCAL_SHARED_LIBRARIES := libbase libcutils libminijail libpackagelistparser
 
 LOCAL_SANITIZE := integer
 LOCAL_CLANG := true
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_STATIC_LIBRARY)
 
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := main.c
+LOCAL_MODULE := sdcard
+LOCAL_CFLAGS := -Wall -Wno-unused-parameter -Werror
+LOCAL_STATIC_LIBRARIES := libsdcard
+LOCAL_SHARED_LIBRARIES := libbase libc libcutils libminijail libpackagelistparser
 include $(BUILD_EXECUTABLE)
