de424d0b5f47293f04d8f8d1230f952e34c07556
dhacker29 <davidhackerdvm@gmail.com>
2013-09-16 00:34:30 -0500

    fs_mgr: Add recover_userdata
    Bridges a gap between GED devices which have a bootloader that formats
    ext4 partitions, and those that do not.
    
    uses board flag TARGET_USERIMAGES_USE_F2FS to add needed source
    
    Change-Id: I0d0b32725e00fca6d18a90c01c3f6e05e0952ed1

--- fs_mgr/fs_mgr.c
+++ fs_mgr/fs_mgr.c
@@ -564,7 +564,21 @@ int fs_mgr_mount_all(struct fstab *fstab)
         } else {
             ERROR("Cannot mount filesystem on %s at %s\n",
                     fstab->recs[i].blk_device, fstab->recs[i].mount_point);
+#ifdef TARGET_USERIMAGES_USE_F2FS
+            if (!strncmp(fstab->recs[i].mount_point, "/data", 5)) {
+                int rc;
+                rc = recover_userdata(fstab->recs[i].fs_type, fstab->recs[i].blk_device, fstab->recs[i].mount_point);
+                if (!rc) {
+                    /* Userdata recovery succeeded, retry this mount. */
+                    i--;
+                    continue;
+                } else {
+                    ERROR("userdata format failed.\n");
+                }
+            }
+#endif
             goto out;
+
         }
     }
 
