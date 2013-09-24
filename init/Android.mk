0b6edaaaf1597992c4230708f447ead8095795dd
David Ng <dave@codeaurora.org>
2013-09-23 18:50:24 -0700

    init: Add vendor-specific initialization hooks.
    Allow optional vendor-specific initializations
    within init.  This can be used for runtime
    initialization setup that init rc scripts do
    not support.
    
    Change-Id: I7623a0d59b18f9ec8e3623958e2f7ccd72b877bf
    
    Former-commit-id: 431ba9e1567a087c4ae83fdef856bf0ef69d832a

--- init/Android.mk
+++ init/Android.mk
@@ -90,4 +90,9 @@ LOCAL_POST_INSTALL_CMD := $(hide) mkdir -p $(TARGET_ROOT_OUT)/sbin; \
     ln -sf ../init $(TARGET_ROOT_OUT)/sbin/watchdogd
 
 LOCAL_SANITIZE := signed-integer-overflow
+
+ifneq ($(strip $(TARGET_INIT_VENDOR_LIB)),)
+LOCAL_WHOLE_STATIC_LIBRARIES += $(TARGET_INIT_VENDOR_LIB)
+endif
+
 include $(BUILD_EXECUTABLE)
