ff05c8ffd67d741d1a86b32e34362e8e317686e3
David Ferguson <ferguson.david@gmail.com>
2013-11-06 21:48:34 -0500

    healthd: allow devices to provide their own libhealthd
      * to use, set TARGET_PROVIDES_LIBHEALTHD := true
    
    Change-Id: I5de702219fce6c0feb94a4c74978d81a1a12ca35

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -4,10 +4,12 @@ ifneq ($(BUILD_TINY_ANDROID),true)
 
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(TARGET_PROVIDES_LIBHEALTHD),true)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_default.cpp
 LOCAL_MODULE := libhealthd.default
 include $(BUILD_STATIC_LIBRARY)
+endif
 
 include $(CLEAR_VARS)
 
