cce64d4cb8c7ade0c8fc0ad654632a14a4229f2a
Matt Mower <mowerm@gmail.com>
2014-01-07 09:56:38 -0600

    adb: do not chown/chmod files pushed to sdcard
    Commit "adb: configure /system file permission with fs_config" can cause
    'Operation not permitted' errors when pushing files to sdcard because
    chown and chmod are not allowed. Omit chown and chmod operations if the
    destination directory has sdcard in the group name.
    
    Change-Id: I05767a2699b152392b4ff1606194826285cbcb1e

--- adb/file_sync_service.c
+++ adb/file_sync_service.c
@@ -21,6 +21,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <grp.h>
 #include <utime.h>
 #include <unistd.h>
 
@@ -37,6 +38,15 @@
 static bool is_on_system(const char *name) {
     const char *SYSTEM = "/system/";
     return (strncmp(SYSTEM, name, strlen(SYSTEM)) == 0);
+}
+
+static bool has_group_sdcard(const char *path) {
+    const char *SDCARD = "sdcard";
+    struct stat info;
+    if(stat(path, &info) != 0)
+        return 0;
+    struct group *gr = getgrgid(info.st_gid);
+    return (strstr(gr->gr_name, SDCARD) != NULL);
 }
 
 static int mkdirs(char *name)
@@ -64,10 +74,12 @@ static int mkdirs(char *name)
             *x = '/';
             return ret;
         } else if(ret == 0) {
-            ret = chown(name, uid, gid);
-            if (ret < 0) {
-                *x = '/';
-                return ret;
+            if(!has_group_sdcard(name)) {
+                ret = chown(name, uid, gid);
+                if (ret < 0) {
+                    *x = '/';
+                    return ret;
+                }
             }
             selinux_android_restorecon(name);
         }
@@ -195,7 +207,7 @@ static int handle_send_file(int s, char *path, unsigned int uid,
         if(fail_errno(s))
             return -1;
         fd = -1;
-    } else {
+    } else if(!has_group_sdcard(path)) {
         if(fchown(fd, uid, gid) != 0) {
             fail_errno(s);
             errno = 0;
