5bc825fd3f727ef6335cc08b44534c342384756d
dhacker29 <dhackerdvm@gmail.com>
2014-01-22 02:09:46 -0600

    init: map ro.boot.radio and device to ro.hw.radio and device
    This fixes baseband initialization on Moto G
    
    Change-Id: I2381f8c4c2c20fdc91690415cde7dffbdb331897

--- init/init.c
+++ init/init.c
@@ -845,6 +845,9 @@ static void export_kernel_boot_props(void)
         { "ro.boot.mode", "ro.bootmode", "unknown", },
         { "ro.boot.baseband", "ro.baseband", "unknown", },
         { "ro.boot.bootloader", "ro.bootloader", "unknown", },
+        { "ro.boot.device", "ro.hw.device", NULL, },
+        { "ro.boot.hwrev", "ro.hw.hwrev", NULL, },
+        { "ro.boot.radio", "ro.hw.radio", NULL, },
     };
 
     for (i = 0; i < ARRAY_SIZE(prop_map); i++) {
