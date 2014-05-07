5dcedf4670a1bbcbe25b63ce93fa398b7651e652
Sravan Kumar Ambapuram <asravan@codeaurora.org>
2014-05-07 11:15:38 +0530

    healthd: increase healthd fast timer to 10mins instead of 1min
    healthd 1 min wakeups are causing significant power impact
    during static display use case. so increasing the timer to 10min.
    
    Change-Id: If7a514e4152789144242e198175918b6687c3bcf

--- healthd/healthd.cpp
+++ healthd/healthd.cpp
@@ -36,7 +36,7 @@
 using namespace android;
 
 // Periodic chores intervals in seconds
-#define DEFAULT_PERIODIC_CHORES_INTERVAL_FAST (60 * 1)
+#define DEFAULT_PERIODIC_CHORES_INTERVAL_FAST (60 * 10)
 #define DEFAULT_PERIODIC_CHORES_INTERVAL_SLOW (60 * 10)
 
 static struct healthd_config healthd_config = {
