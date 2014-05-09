90e193c7218e08ad73ad854070be7757624d1ffb
dhacker29 <dhackerdvm@gmail.com>
2014-05-09 01:12:37 -0400

    Allow charger to build with recovery-cm
    For some reason pahtmap is not working and charger still includes
    bootable/recovery/minui/minui.h
    
    Change-Id: I88412bc1b55383bb1e0f52793e5327e98ce7c777

--- charger/Android.mk
+++ charger/Android.mk
@@ -22,7 +22,11 @@ LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_UNSTRIPPED)
 
-LOCAL_C_INCLUDES := $(call include-path-for, recovery)
+ifneq ($(RECOVERY_VARIANT),)
+    LOCAL_C_INCLUDES := bootable/recovery-$(RECOVERY_VARIANT)
+else
+    LOCAL_C_INCLUDES := bootable/recovery
+endif
 
 LOCAL_STATIC_LIBRARIES := libminui libpixelflinger_static libpng
 ifeq ($(strip $(BOARD_CHARGER_ENABLE_SUSPEND)),true)
