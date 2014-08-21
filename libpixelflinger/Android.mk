28cf6c25cda2eacee759cb652a42225e1983f7db
HazouPH <jgrharbers@gmail.com>
2014-08-21 15:12:08 +0200

    [x86] Software-accelerated Pixel Flinger
    This patch improves the speed of pixelflinger needed executables (recovery). It is slightly changed from the original source from Motorola that came with source code from the Razr I. It fixes the issue with 'lag' within the ui of all x86 build recovery's.
    
    Original source:
    http://sourceforge.net/projects/razr-i.motorola/files/razr-i/9.8.2I-50_SMI-28/system_core.tar.gz/download
    
    Android-ia implementation off the same code:
    https://github.com/android-ia/platform_system_core/commit/8d7070878d582206abe1e10461ccbd8005a09008
    
    Change-Id: Ica99f16f87f1dedb8dc8bfbc17a0deb9f3f3962f

--- libpixelflinger/Android.mk
+++ libpixelflinger/Android.mk
@@ -6,27 +6,51 @@ include $(CLEAR_VARS)
 #
 
 include $(CLEAR_VARS)
-PIXELFLINGER_SRC_FILES:= \
+PIXELFLINGER_SRC_FILES += \
+    codeflinger/CodeCache.cpp \
+    codeflinger/tinyutils/SharedBuffer.cpp \
+    codeflinger/tinyutils/VectorImpl.cpp \
+    format.cpp \
+    clear.cpp \
+    raster.cpp \
+    buffer.cpp
+
+ifneq ($(TARGET_ARCH),x86)
+PIXELFLINGER_SRC_FILES += \
     codeflinger/ARMAssemblerInterface.cpp \
     codeflinger/ARMAssemblerProxy.cpp \
     codeflinger/ARMAssembler.cpp \
-    codeflinger/CodeCache.cpp \
     codeflinger/GGLAssembler.cpp \
     codeflinger/load_store.cpp \
     codeflinger/blending.cpp \
     codeflinger/texturing.cpp \
     codeflinger/disassem.c \
-	codeflinger/tinyutils/SharedBuffer.cpp \
-	codeflinger/tinyutils/VectorImpl.cpp \
-	fixed.cpp.arm \
-	picker.cpp.arm \
-	pixelflinger.cpp.arm \
-	trap.cpp.arm \
-	scanline.cpp.arm \
-	format.cpp \
-	clear.cpp \
-	raster.cpp \
-	buffer.cpp
+    fixed.cpp.arm \
+    picker.cpp.arm \
+    pixelflinger.cpp.arm \
+    trap.cpp.arm \
+    scanline.cpp.arm
+endif
+
+ifeq ($(TARGET_ARCH),x86)
+
+PIXELFLINGER_SRC_FILES +=  \
+    codeflinger/x86/X86Assembler.cpp \
+    codeflinger/x86/GGLX86Assembler.cpp \
+    codeflinger/x86/load_store.cpp \
+    codeflinger/x86/blending.cpp \
+    codeflinger/x86/texturing.cpp \
+    fixed.cpp \
+    picker.cpp \
+    pixelflinger.cpp \
+    trap.cpp \
+    scanline.cpp \
+
+LOCAL_C_INCLUDES := \
+    dalvik/vm/compiler/codegen/x86/libenc
+
+LOCAL_STATIC_LIBRARIES += libenc
+endif
 
 ifeq ($(TARGET_ARCH),arm)
 ifeq ($(TARGET_ARCH_VERSION),armv7-a)
@@ -76,6 +100,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE:= libpixelflinger_static
 LOCAL_SRC_FILES := $(PIXELFLINGER_SRC_FILES)
 LOCAL_CFLAGS := $(PIXELFLINGER_CFLAGS)
+ifeq ($(TARGET_ARCH),x86)
+LOCAL_C_INCLUDES := \
+    dalvik/vm/compiler/codegen/x86/libenc
+LOCAL_STATIC_LIBRARIES += libenc
+endif
 include $(BUILD_STATIC_LIBRARY)
 
 
