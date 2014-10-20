4e12a73ead3116cde462da23dc6ef9b456094f74
Jianxun Zhang <jianxun.zhang@intel.com>
2014-10-20 10:30:10 -0700

    Turn a shutdown request into reboot when charger is connected.
    Not all of the PMIC support to boot device automatically into
    charge-only mode after shutdown when charger is connected.
    However, we have to workaround this issue that should be done
    in HW/PMIC in system/core project:
    () There is no "reboot" HAL for vendor-specific change. Reboot
    and shutdown are based on syscall provided by Linux kernel.
    () A change in upper layers in Java will potentially miss some
    corner cases since any permitted pieces in system is capable to
    set the sys.powerctl property to shutdown device.
    () The healthd is the "sweet spot" where we can get connection
    of charger(s) between kernel and Android framework.
    () The init is the "sweet spot" where we can play this trick
    and limit it in Android mode in the whole reboot/shutdown
    execution flow. Some native non-android daemons that calls
    reboot API at a lower level won't be effected.
    () Putting the above work in kernel cannot help much:
      - We will have to hack the standard reboot routines in kernel.
      - It is also difficult to differentiate shutdown requests
        from user space.
      - Acquire charger(s) status in reboot sub-system will actually
        be a re-implementation of what healthd in user space does.
    In a short, this change tries to resolve the issue in a least
    destructive way.
    Two properties are invented. One is to enable/disable the
    overriding. Another is connection status of charger(s).
    
    Change-Id: Ie96fb58ecb9a4e8ffe7e73e590483b5323e40b74
    Signed-off-by: Jianxun Zhang <jianxun.zhang@intel.com>

--- healthd/BatteryMonitor.cpp
+++ healthd/BatteryMonitor.cpp
@@ -42,6 +42,8 @@
 #define FAKE_BATTERY_TEMPERATURE 424
 #define ALWAYS_PLUGGED_CAPACITY 100
 
+#define CHARGER_CONNECTION_PROP "sys.charger.connected"
+
 namespace android {
 
 struct sysfsStringEnumMap {
@@ -56,6 +58,16 @@ static int mapSysfsString(const char* str,
             return map[i].val;
 
     return -1;
+}
+
+static void updateChargerStatusProp (bool status) {
+    const char* charger_status;
+    char prop[PROP_VALUE_MAX] = {0};
+    charger_status = status ? "1" : "";
+
+    if (property_get(CHARGER_CONNECTION_PROP, prop, NULL) <= 0
+            || strcmp(prop, charger_status))
+        property_set(CHARGER_CONNECTION_PROP, charger_status);
 }
 
 int BatteryMonitor::getBatteryStatus(const char* status) {
@@ -184,6 +196,7 @@ int BatteryMonitor::getIntField(const String8& path) {
 
 bool BatteryMonitor::update(void) {
     bool logthis;
+    bool chargerConnected;
 
     props.chargerAcOnline = false;
     props.chargerUsbOnline = false;
@@ -401,9 +414,12 @@ bool BatteryMonitor::update(void) {
         KLOG_WARNING(LOG_TAG, "%s\n", dmesgline);
     }
 
-    healthd_mode_ops->battery_update(&props);
-    return props.chargerAcOnline | props.chargerUsbOnline |
+    chargerConnected = props.chargerAcOnline | props.chargerUsbOnline |
             props.chargerWirelessOnline | props.chargerDockAcOnline;
+    updateChargerStatusProp(chargerConnected);
+
+    healthd_mode_ops->battery_update(&props);
+    return chargerConnected;
 }
 
 status_t BatteryMonitor::getProperty(int id, struct BatteryProperty *val) {
