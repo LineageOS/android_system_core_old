75d2031adcd3fc3c476392de6f840f0e0a2fdc48
pyler <pyler.inbox@gmail.com>
2014-11-02 12:30:58 +0000

    Revert "core: Correct the sensors UID"
    Broken sensors on many devices (G2, N5, ..)
    
    This reverts commit 9aac9eaa5722bb152b33469d9b95b7848b048239.
    
    Change-Id: Ie19c9e3831ff6aee163953e6c11a53ccb5230ff2

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -96,8 +96,7 @@
 #define AID_NET_BT_STACK  3008  /* bluetooth: access config files */
 #define AID_QCOM_ONCRPC   3009  /* can read/write /dev/oncrpc files */
 #define AID_QCOM_DIAG     3010  /* can read/write /dev/diag */
-#define AID_SENSORS       3011 /* access to /dev/socket/sensor_ctl_socket & QCCI/QCSI */
-#define AID_IMS           3012  /* can read/write /dev/socket/imsrtp */
+#define AID_IMS           3011  /* can read/write /dev/socket/imsrtp */
 
 #if defined(MOTOROLA_UIDS)
 #define AID_MOT_OSH       5000  /* OSH */
@@ -114,6 +113,8 @@
 #define AID_MOT_CAIF      9010  /* can create CAIF sockets */
 #define AID_MOT_DLNA      9011  /* DLNA native */
 #endif // MOTOROLA_UIDS
+
+#define AID_SENSORS       3012 /* access to /dev/socket/sensor_ctl_socket & QCCI/QCSI */
 
 #define AID_MISC          9998  /* access to misc storage */
 #define AID_NOBODY        9999
