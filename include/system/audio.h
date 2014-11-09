redmi2/android_system_core

system/core: Add support for JOC format
Add support for EAC3 JOC format to core. Add EAC3 JOC format as a
valid format.

Change-Id: I2a396a570421c66df84a874cde6f3616392c6ba1

include/system/audio.h
@@ -184,6 +184,7 @@ typedef enum {
     AUDIO_FORMAT_EVRCNW              = 0x16000000UL,
     AUDIO_FORMAT_PCM_OFFLOAD         = 0x17000000UL,
     AUDIO_FORMAT_FLAC                = 0x18000000UL,
+    AUDIO_FORMAT_E_AC3_JOC           = 0x19000000UL,
     AUDIO_FORMAT_MAIN_MASK           = 0xFF000000UL,
     AUDIO_FORMAT_SUB_MASK            = 0x00FFFFFFUL,
 
@@ -705,6 +706,7 @@ static inline bool audio_is_valid_format(audio_format_t format)
     case AUDIO_FORMAT_MP2:
     case AUDIO_FORMAT_EVRCNW:
     case AUDIO_FORMAT_FLAC:
+    case AUDIO_FORMAT_E_AC3_JOC:
         return true;
     case AUDIO_FORMAT_PCM_OFFLOAD:
         if (format != AUDIO_FORMAT_PCM_16_BIT_OFFLOAD &&

LineageOS/android_system_core_old

include/system/audio.h
@@ -282,7 +282,7 @@ typedef enum {
     AUDIO_FORMAT_EVRCNW              = 0x1B000000UL,
     AUDIO_FORMAT_PCM_OFFLOAD         = 0x1C000000UL,
     AUDIO_FORMAT_FLAC                = 0x1D000000UL,
-
+    AUDIO_FORMAT_E_AC3_JOC           = 0x1E000000UL,
     AUDIO_FORMAT_MAIN_MASK           = 0xFF000000UL,
     AUDIO_FORMAT_SUB_MASK            = 0x00FFFFFFUL,
 
@@ -1409,6 +1409,7 @@ static inline bool audio_is_valid_format(audio_format_t format)
     case AUDIO_FORMAT_MP2:
     case AUDIO_FORMAT_EVRCNW:
     case AUDIO_FORMAT_FLAC:
+    case AUDIO_FORMAT_E_AC3_JOC:
         return true;
     case AUDIO_FORMAT_PCM_OFFLOAD:
         if (format != AUDIO_FORMAT_PCM_16_BIT_OFFLOAD &&