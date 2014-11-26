607422153bdcde6ac99ffc3458cc5148900328a8
Tom Marshall <tdm@cyngn.com>
2014-11-26 13:48:46 -0800

    adb: Look for bu in /sbin when in recovery mode
     * Restore global variable recovery_mode.  This is set based on the
       commandline parameter --banner.
    
     * Use recovery_mode to select path to 'bu' binary for both backup and
       restore services.
    
    Change-Id: Ie4c945d00601514d7f16357bae10ff7f232633e1

--- adb/adb.cpp
+++ adb/adb.cpp
@@ -54,6 +54,8 @@ int HOST = 0;
 
 #if !ADB_HOST
 const char *adb_device_banner = "device";
+
+int recovery_mode = 0;
 #endif
 
 void fatal(const char *fmt, ...)
@@ -194,6 +196,8 @@ void adb_trace_init() {
 
 #if !ADB_HOST
     start_device_log();
+
+    recovery_mode = (strcmp(adb_device_banner, "recovery") == 0);
 #endif
 }
 
