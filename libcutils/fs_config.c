da5937b4d7cfddb07aaa5e48ab122740924abe92
Ricardo Cerqueira <ricardo@cyngn.com>
2014-11-26 21:49:52 +0000

    Update permissions to the superuser binary
    Change-Id: Ia382c74854cfbe625653b8b6b016b37f93132658

--- libcutils/fs_config.c
+++ libcutils/fs_config.c
@@ -134,9 +134,8 @@ static const struct fs_path_config android_files[] = {
     { 00750, AID_ROOT,      AID_SHELL,     0, "data/nativetest/*" },
     { 00750, AID_ROOT,      AID_SHELL,     0, "data/nativetest64/*" },
 
-    /* the following two files are INTENTIONALLY set-uid, but they
-     * are NOT included on user builds. */
-    { 04750, AID_ROOT,      AID_SHELL,     0, "system/xbin/su" },
+    /* CM's daemonized su doesn't need the setuid bit */
+    { 00755, AID_ROOT,      AID_SHELL,     0, "system/xbin/su" },
     { 06755, AID_ROOT,      AID_ROOT,      0, "system/xbin/procmem" },
 
     /* the following files have enhanced capabilities and ARE included in user builds. */
