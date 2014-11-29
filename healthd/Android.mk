23ea3b2647bf015633b3cb9fbf3a9c5b808e0aad
Shareef Ali <shareefalis@cyanogenmod.org>
2014-11-29 02:51:53 -0600

    Core: fix compile issue of healthd.`
    fixes this in darwin cp: symlink: /sbin/healthd: File exists
    
    Change-Id: Ia0a51962ec6d1713ece1e9c1d4f9df479fee5f5b

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -54,7 +54,7 @@ LOCAL_HAL_STATIC_LIBRARIES := libhealthd
 
 # Symlink /charger to /sbin/healthd
 LOCAL_POST_INSTALL_CMD := $(hide) mkdir -p $(TARGET_ROOT_OUT) \
-    && ln -sf /sbin/healthd $(TARGET_ROOT_OUT)/charger
+    && rm -f $(TARGET_ROOT_OUT)/charger && ln -sf /sbin/healthd $(TARGET_ROOT_OUT)/charger
 
 include $(BUILD_EXECUTABLE)
 
