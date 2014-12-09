2c2bda32508049d842cf0ece4e654394e414dd50
Howard Su <howard0su@gmail.com>
2014-12-09 16:54:00 +0800

    Fix build error under osx.
    warning is generated by the deprecated API:
    GetCurrentProcess
    
    Change-Id: I6ab6b62e9b0f40b1315edbdf32da9c61a4731d51

--- fastboot/Android.mk
+++ fastboot/Android.mk
@@ -22,7 +22,7 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../mkbootimg \
 LOCAL_SRC_FILES := protocol.c engine.c bootimg.c fastboot.c util.c fs.c
 LOCAL_MODULE := fastboot
 LOCAL_MODULE_TAGS := debug
-LOCAL_CFLAGS += -std=gnu99 -Werror
+LOCAL_CFLAGS += -std=gnu99
 
 ifeq ($(HOST_OS),linux)
   LOCAL_SRC_FILES += usb_linux.c util_linux.c
