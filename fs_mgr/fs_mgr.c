4f3d2da28cb1398872dcbbe3860ccda4eaa86f1e
Dan Pasanen <dan.pasanen@gmail.com>
2015-01-13 10:55:30 -0600

    fs_mgr: auto-detect filesystem when performing a check
    * When multiple partition types are specified in fstab with check
      enabled on them, fs_mgr attempts to check them based on the fstab
      entry it is currently looking at regardless of the filesystem type
      the volume actually is. This causes an issue because e2fsck run
      on an f2fs partition really screws it up pretty bad.  it turns it
      into an ext4 volume.
    
    * We want to still be able to check our filesystems though, so link
      against blkid here and attempt to autodetect the filesystem. If one
      is found and it differs from the current fstab type, bail on the check.
    
    Change-Id: I1bc1a1aad3328d139ab0d3d81b8b8c4b1b6bc49f

--- fs_mgr/fs_mgr.c
+++ fs_mgr/fs_mgr.c
@@ -35,6 +35,7 @@
 #include <cutils/partition_utils.h>
 #include <cutils/properties.h>
 #include <logwrap/logwrap.h>
+#include <blkid/blkid.h>
 
 #include "mincrypt/rsa.h"
 #include "mincrypt/sha.h"
@@ -97,6 +98,15 @@ static void check_fs(char *blk_device, char *fs_type, char *target)
         "-y",
         blk_device
     };
+
+    char *detected_fs_type = blkid_get_tag_value(NULL, "TYPE", blk_device);
+
+    if (detected_fs_type && strcmp(fs_type, detected_fs_type) != 0) {
+        INFO("%s: Detected %s on %s but attempted to fsck as %s\n", __func__,
+             detected_fs_type, blk_device, fs_type);
+        INFO("%s: Skipping check on %s\n", __func__, blk_device);
+        return;
+    }
 
     /* Check for the types of filesystems we know how to check */
     if (!strcmp(fs_type, "ext2") || !strcmp(fs_type, "ext3") || !strcmp(fs_type, "ext4")) {
--- init/Android.mk
+++ init/Android.mk
@@ -60,7 +60,9 @@ LOCAL_STATIC_LIBRARIES := \
 	libc \
 	libselinux \
 	libmincrypt \
-	libext4_utils_static
+	libext4_utils_static \
+	libext2_blkid \
+	libext2_uuid
 
 LOCAL_ADDITIONAL_DEPENDENCIES += $(LOCAL_PATH)/Android.mk
 ifneq ($(strip $(TARGET_PLATFORM_DEVICE_BASE)),)
