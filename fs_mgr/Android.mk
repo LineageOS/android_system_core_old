70d19d8c0ef2ad5d735ce3e0abe880ebdaeed6c4
Dan Pasanen <dan.pasanen@gmail.com>
2015-01-28 16:41:29 -0800

    fs_mgr: Skip filesystem check unless fs_type matches
    * Prevent accidentally destroying a partition of the wrong type.
    * Don't skip iterations of the internal mount_all loop, otherwise
      encryptability may not be properly handled.
    
    Change-Id: I8f3ddc396a5fb85f4ae0a0a11dd61fb4d6462d6d

--- fs_mgr/Android.mk
+++ fs_mgr/Android.mk
@@ -8,8 +8,8 @@ LOCAL_SRC_FILES:= fs_mgr.c fs_mgr_verity.c fs_mgr_fstab.c
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 
 LOCAL_MODULE:= libfs_mgr
-LOCAL_STATIC_LIBRARIES := liblogwrap libmincrypt libext4_utils_static
-LOCAL_C_INCLUDES += system/extras/ext4_utils
+LOCAL_STATIC_LIBRARIES := liblogwrap libmincrypt libext4_utils_static libext2_blkid libext2_uuid
+LOCAL_C_INCLUDES += system/extras/ext4_utils external/e2fsprogs/lib
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
 LOCAL_CFLAGS := -Werror
 
@@ -30,7 +30,7 @@ LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)/sbin
 LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_UNSTRIPPED)
 
-LOCAL_STATIC_LIBRARIES := libfs_mgr liblogwrap libcutils liblog libc libmincrypt libext4_utils_static
+LOCAL_STATIC_LIBRARIES := libfs_mgr liblogwrap libcutils liblog libc libmincrypt libext4_utils_static libext2_blkid libext2_uuid
 
 LOCAL_CFLAGS := -Werror
 
