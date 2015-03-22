29f7779fdbccb5926a3f74d70c8721fa62e75a0c
Scott Mertz <scott@cyngn.com>
2015-03-21 17:33:47 -0700

    healthd: allow custom charger
    Change-Id: I1380bea300bfaa6819d50bdc813329c93050286a
    (cherry picked from commit b6ebd65cd36d06daa5cf33f031d7e03b0bd674c6)

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -13,9 +13,14 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
 	healthd.cpp \
 	healthd_mode_android.cpp \
-	healthd_mode_charger.cpp \
 	BatteryMonitor.cpp \
 	BatteryPropertiesRegistrar.cpp
+
+ifeq ($(strip $(BOARD_HEALTHD_CUSTOM_CHARGER)),)
+  LOCAL_SRC_FILES += healthd_mode_charger.cpp
+else
+  LOCAL_SRC_FILES += ../../../$(BOARD_HEALTHD_CUSTOM_CHARGER)
+endif
 
 LOCAL_MODULE := healthd
 LOCAL_MODULE_TAGS := optional
@@ -75,8 +80,13 @@ include $$(BUILD_PREBUILT)
 endef
 
 _img_modules :=
+ifeq ($(strip $(BOARD_HEALTHD_CUSTOM_CHARGER_RES)),)
+IMAGES_DIR := images
+else
+IMAGES_DIR := ../../../$(BOARD_HEALTHD_CUSTOM_CHARGER_RES)
+endif
 _images :=
-$(foreach _img, $(call find-subdir-subdir-files, "images", "*.png"), \
+$(foreach _img, $(call find-subdir-subdir-files, "$(IMAGES_DIR)", "*.png"), \
   $(eval $(call _add-charger-image,$(_img))))
 
 include $(CLEAR_VARS)
