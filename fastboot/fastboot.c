2e5c51a7597a53405349d11586524948f55c434a
Chao Chen <cchen@cyngn.com>
2015-04-29 14:59:52 -0700

    fastboot: Add MMX fastboot USB ID
    Change-Id: I9cdbac6054e7f081411c34f641874e2aa65049a6

--- fastboot/fastboot.c
+++ fastboot/fastboot.c
@@ -216,7 +216,8 @@ int match_fastboot_with_serial(usb_ifc_info *info, const char *local_serial)
        (info->dev_vendor != 0x0b05) &&  // Asus
        (info->dev_vendor != 0x0bb4) &&  // HTC
        (info->dev_vendor != 0x0421) &&  // Nokia
-       (info->dev_vendor != 0x1ebf))    // Coolpad
+       (info->dev_vendor != 0x1ebf) &&  // Coolpad
+       (info->dev_vendor != 0x2a96))    // MMX
             return -1;
     if(info->ifc_class != 0xff) return -1;
     if(info->ifc_subclass != 0x42) return -1;
