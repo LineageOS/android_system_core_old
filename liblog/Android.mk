e575b4ab70d0524a1faf6c2e768aa409382cec22
Thomas Wendt <thoemy@gmx.net>
2015-05-20 20:42:01 +0200

    libcutils/liblog: Add functions required by Huawei g620_a2 binaries
    Change-Id: I58604686008340ef4e10f3f67dc329fe7bd864cc

--- liblog/Android.mk
+++ liblog/Android.mk
@@ -74,6 +74,10 @@ ifeq ($(TARGET_USES_MOTOROLA_LOG),true)
 LIBLOG_CFLAGS := -DMOTOROLA_LOG
 endif
 
+ifeq ($(TARGET_USES_HUAWEI_LOG),true)
+LIBLOG_CFLAGS := -DHUAWEI_LOG
+endif
+
 # Shared and static library for target
 # ========================================================
 include $(CLEAR_VARS)
