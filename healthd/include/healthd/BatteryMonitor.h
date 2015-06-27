422f671a1d816e8b4e53296c4151a58371833507
Jorge Ruesga <jorge@ruesga.com>
2015-06-28 00:55:25 +0200

    healthd: dock battery
    Change-Id: I2ad09e5e87d55c47af4d1efc14cc585cc08e2dce
    Require: topic:dock_battery
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- healthd/include/healthd/BatteryMonitor.h
+++ healthd/include/healthd/BatteryMonitor.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013 The Android Open Source Project
+ * Copyright (C) 2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,7 +35,9 @@ class BatteryMonitor {
         ANDROID_POWER_SUPPLY_TYPE_AC,
         ANDROID_POWER_SUPPLY_TYPE_USB,
         ANDROID_POWER_SUPPLY_TYPE_WIRELESS,
-        ANDROID_POWER_SUPPLY_TYPE_BATTERY
+        ANDROID_POWER_SUPPLY_TYPE_BATTERY,
+        ANDROID_POWER_SUPPLY_TYPE_DOCK_AC,
+        ANDROID_POWER_SUPPLY_TYPE_DOCK_BATTERY
     };
 
     BatteryMonitor();
@@ -42,6 +45,7 @@ class BatteryMonitor {
     bool update(void);
     int getChargeStatus();
     status_t getProperty(int id, struct BatteryProperty *val);
+    status_t getDockProperty(int id, struct BatteryProperty *val);
     void dumpState(int fd);
 
   private:
@@ -49,6 +53,7 @@ class BatteryMonitor {
     Vector<String8> mChargerNames;
     bool mBatteryDevicePresent;
     bool mAlwaysPluggedDevice;
+    bool mDockBatteryDevicePresent;
     int mBatteryFixedCapacity;
     int mBatteryFixedTemperature;
     struct BatteryProperties props;
