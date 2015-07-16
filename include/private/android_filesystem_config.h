b198304a56bad86a08b5dd647113c61489dcb564
Sreelakshmi Gownipalli <sgownipa@codeaurora.org>
2015-07-16 12:22:52 -0700

    diag: Add new qcom_diag group to access /dev/diag
    This change adds the definition of qcom_diag and
    also modify the /dev/diag node group to qcom_diag.
    
    Change-Id: I07bd099c76f3fd2685f3a6698e37314a0a2c8f44

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -97,6 +97,7 @@
 #define AID_NET_BW_STATS  3006  /* read bandwidth statistics */
 #define AID_NET_BW_ACCT   3007  /* change bandwidth statistics accounting */
 #define AID_NET_BT_STACK  3008  /* bluetooth: access config files */
+#define AID_QCOM_DIAG     3009  /* can read/write /dev/diag */
 
 #define AID_RFS           3012  /* Remote Filesystem for peripheral processors */
 #define AID_RFS_SHARED    3013  /* Shared files for Remote Filesystem for peripheral processors  */
@@ -175,6 +176,7 @@ static const struct android_id_info android_ids[] = {
     { "shell",         AID_SHELL, },
     { "cache",         AID_CACHE, },
     { "diag",          AID_DIAG, },
+    { "qcom_diag",     AID_QCOM_DIAG, },
 
     { "net_bt_admin",  AID_NET_BT_ADMIN, },
     { "net_bt",        AID_NET_BT, },
