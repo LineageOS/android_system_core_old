1551c7acfb8801b085ad049b46029d3e04ac7836
Steve Kondik <steve@cyngn.com>
2015-09-20 17:36:54 -0700

    cutils: Remove ioprio magic
     * ..and replace it with something we can call explictly where it is
       required in order to achieve the same goodness without the badness.
    
    Change-Id: I605b3acbc56ce82663dd6f1c9f3320d0ab75e178

--- libutils/Threads.cpp
+++ libutils/Threads.cpp
@@ -42,7 +42,6 @@
 #include <utils/threads.h>
 #include <utils/Log.h>
 
-#include <cutils/iosched_policy.h>
 #include <cutils/sched_policy.h>
 
 #ifdef HAVE_ANDROID_OS
@@ -325,8 +324,6 @@ int androidSetThreadPriority(pid_t tid, int pri)
     } else {
         errno = lasterr;
     }
-
-    android_set_bfqio_prio(tid, pri);
 #endif
 
     return rc;
@@ -712,12 +709,6 @@ status_t Thread::run(const char* name, int32_t priority, size_t stack)
 
         return UNKNOWN_ERROR;
     }
-
-#ifdef HAVE_ANDROID_OS
-    pid_t tid = __pthread_gettid(android_thread_id_t_to_pthread(mThread));
-    if (tid >= 200)
-        android_set_bfqio_prio(tid, priority);
-#endif
 
     // Do not refer to mStatus here: The thread is already running (may, in fact
     // already have exited with a valid mStatus result). The NO_ERROR indication
