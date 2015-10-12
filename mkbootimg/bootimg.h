0fcbde444948c6851d08a2c4de0a44d555f112f5
rbox <androidrbox@gmail.com>
2015-10-11 17:29:09 -0700

    mkbootimg: Fix bootimg structure after adding --dt parameter
    396224f2caf2fadf7b35239e7daf87139824e3e7 added dt_size to bootimg.h,
    but didn't fix the unused field, so cmdline was shifted by 4
    bytes from where it was supposed to be.
    
    Change-Id: I112b735971dc75ed7feacfb849ef1b76858caa96

--- mkbootimg/bootimg.h
+++ mkbootimg/bootimg.h
@@ -44,7 +44,7 @@ struct boot_img_hdr
     uint32_t tags_addr;    /* physical addr for kernel tags */
     uint32_t page_size;    /* flash page size we assume */
     uint32_t dt_size;      /* device tree in bytes */
-    uint32_t unused[2];    /* future expansion: should be 0 */
+    uint32_t unused;       /* future expansion: should be 0 */
 
     uint8_t name[BOOT_NAME_SIZE]; /* asciiz product name */
 
