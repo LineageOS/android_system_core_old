0951785b62a09abfd80b4a5135e72c2792e83239
Aaron Kling <webgeek1234@gmail.com>
2015-11-12 12:20:25 -0600

    Add vendor hook to handle_control_message
    This is needed for Nvidia Shield devices to handle a
    'restart consolemode' request from a blobbed stock app.
    
    The vendor function is expected to return non-0 if it did
    not handle the message and 0 if it did (or wants it ignored).
    
    Change-Id: I3f128ec3d3a91ae94adaa9a819981916736a802d

--- init/vendor_init.c
+++ init/vendor_init.c
@@ -35,3 +35,9 @@ __attribute__ ((weak))
 void vendor_load_properties()
 {
 }
+
+__attribute__ ((weak))
+int vendor_handle_control_message(const char *msg, const char *arg)
+{
+    return -ENOSYS;
+}
