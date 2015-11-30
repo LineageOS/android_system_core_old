7519fec0bbbd557c31577d694c7168b441fe1fe3
Ethan Chen <intervigil@gmail.com>
2015-11-30 15:47:04 -0800

    healthd: Actually use correct common backlight function
    Change-Id: I4f97007bf1c30626c943e163189a02a58f5c9e3d

--- healthd/healthd_mode_charger.cpp
+++ healthd/healthd_mode_charger.cpp
@@ -608,7 +608,7 @@ static void process_key(struct charger *charger, int code, int64_t now)
                    accordingly. */
                 if (property_get_bool("ro.enable_boot_charger_mode", false)) {
                     LOGW("[%" PRId64 "] booting from charger mode\n", now);
-                    healthd_board_mode_set_backlight(false);
+                    healthd_board_mode_charger_set_backlight(false);
                     gr_fb_blank(true);
                     property_set("sys.boot_from_charger_mode", "1");
                 } else {
