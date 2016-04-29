633ca467fed504e72fbb6a1fd4dc0949f2ea79e7
Vikram Mulukutla <markivx@codeaurora.org>
2016-04-29 13:01:38 -0700

    Add system-background cgroup to the schedtune controller hierarchy.
    Change-Id: Ic6f06defd0065c0580a4bca14ac7d5351aca1072

--- libcutils/sched_policy.cpp
+++ libcutils/sched_policy.cpp
@@ -64,6 +64,7 @@ static int bg_schedboost_fd = -1;
 static int fg_schedboost_fd = -1;
 static int ta_schedboost_fd = -1;
 static int rt_schedboost_fd = -1;
+static int system_bg_schedboost_fd = -1;
 
 /* Add tid to the scheduling group defined by the policy */
 static int add_tid_to_cgroup(int tid, int fd)
@@ -162,6 +163,8 @@ static void __initialize() {
                 bg_schedboost_fd = open(filename, O_WRONLY | O_CLOEXEC);
                 filename = "/dev/stune/rt/tasks";
                 rt_schedboost_fd = open(filename, O_WRONLY | O_CLOEXEC);
+                filename = "/dev/stune/system-background/tasks";
+                system_bg_schedboost_fd = open(filename, O_WRONLY | O_CLOEXEC);
             }
         }
     }
@@ -308,6 +311,7 @@ int set_cpuset_policy(int tid, SchedPolicy policy)
         break;
     case SP_SYSTEM:
         fd = system_bg_cpuset_fd;
+        boost_fd = system_bg_schedboost_fd;
         break;
     default:
         boost_fd = fd = -1;
