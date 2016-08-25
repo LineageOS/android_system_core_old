900f7f745030ab835a3dc0ccba641894937a4f14
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-08-25 23:37:44 +0200

    Revert "healthd: dock battery"
    This reverts commit 798a84d32d47b9ce4a704d4cf139bd3e9260360e.
    
    Change-Id: If42d90d69ec38d56e9a430a41e626c58a238c549

--- healthd/BatteryPropertiesRegistrar.h
+++ healthd/BatteryPropertiesRegistrar.h
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2013 The Android Open Source Project
- * Copyright (C) 2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,7 +40,6 @@ private:
     void registerListener(const sp<IBatteryPropertiesListener>& listener);
     void unregisterListener(const sp<IBatteryPropertiesListener>& listener);
     status_t getProperty(int id, struct BatteryProperty *val);
-    status_t getDockProperty(int id, struct BatteryProperty *val);
     status_t dump(int fd, const Vector<String16>& args);
     void binderDied(const wp<IBinder>& who);
 };
