c5b98b03d5b0e608835134911379ca375d2dc7fe
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-08-26 18:23:33 +0200

    healthd: Fix libhealthd.qcom build
    Change-Id: I3bd08cc55501a5a43809837dc5b185817fdd73aa

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -22,6 +22,8 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_msm.cpp
 LOCAL_MODULE := libhealthd.qcom
 LOCAL_CFLAGS := -Werror
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
