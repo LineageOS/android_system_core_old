5223f4cbb274261aa47d94d07ec40db7df600a2c
Hriday Sharma <hridaysharma42@gmail.com>
2016-09-02 21:07:57 +0530

    healthd/healthd_board_default.cpp: remove redefinitions
    * Fix error
    
    system/core/healthd/healthd_board_default.cpp:51:6: error: redefinition of 'healthd_board_mode_charger_draw_battery'
    void healthd_board_mode_charger_draw_battery(struct android::BatteryProperties*)
    
    system/core/healthd/healthd_board_default.cpp:56:6: error: redefinition of 'healthd_board_mode_charger_battery_update'
    void healthd_board_mode_charger_battery_update(struct android::BatteryProperties*)
    
    system/core/healthd/healthd_board_default.cpp:61:6: error: redefinition of 'healthd_board_mode_charger_set_backlight'
    void healthd_board_mode_charger_set_backlight(bool)
    
    system/core/healthd/healthd_board_default.cpp:66:6: error: redefinition of 'healthd_board_mode_charger_init'
    void healthd_board_mode_charger_init()
    
    Change-Id: Ibc8dee67d9b7d205be84244146b989af075dafbc

--- healthd/healthd_board_default.cpp
+++ healthd/healthd_board_default.cpp
@@ -33,32 +33,12 @@ void healthd_board_mode_charger_draw_battery(struct android::BatteryProperties*)
 
 }
 
-void healthd_board_mode_charger_battery_update(struct android::BatteryProperties*)
-{
-
-}
-
 void healthd_board_mode_charger_set_backlight(bool)
 {
 
 }
 
-void healthd_board_mode_charger_init()
-{
-
-}
-
-void healthd_board_mode_charger_draw_battery(struct android::BatteryProperties*)
-{
-
-}
-
 void healthd_board_mode_charger_battery_update(struct android::BatteryProperties*)
-{
-
-}
-
-void healthd_board_mode_charger_set_backlight(bool)
 {
 
 }
