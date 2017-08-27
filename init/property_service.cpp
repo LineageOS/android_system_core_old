927f82869995c5b9458f2397ee6912cde5958109
LuK1337 <priv.luk@gmail.com>
2017-08-27 23:18:44 +0200

    init: Add missing call to vendor_load_properties
    Change-Id: I8f75a2d9423045927bcbc4d0e2d41c2d43aefa53

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -661,6 +661,13 @@ void load_system_props() {
     load_properties_from_file("/odm/build.prop", NULL);
     load_properties_from_file("/vendor/build.prop", NULL);
     load_properties_from_file("/factory/factory.prop", "ro.*");
+
+    /*
+     * update with vendor-specific property runtime
+     * overrides
+     */
+    vendor_load_properties();
+
     load_recovery_id_prop();
 }
 
