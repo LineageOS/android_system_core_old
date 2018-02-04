b0d9b00aaee2f5972b1666d4b76e3eeba17354ba
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-02-04 13:28:37 +0100

    fs_mgr: Bring back cm behavior of partition footer
    Change-Id: I05019e5f6ee5aa67f876dcde1c9ce063fb0849ae

--- fs_mgr/fs_mgr_format.cpp
+++ fs_mgr/fs_mgr_format.cpp
@@ -53,20 +53,25 @@ static int get_dev_sz(char *fs_blkdev, uint64_t *dev_sz)
     return 0;
 }
 
-static int format_ext4(char *fs_blkdev, char *fs_mnt_point, bool crypt_footer)
+static int format_ext4(char *fs_blkdev, char *fs_mnt_point, uint64_t dev_sz, uint64_t footer, bool crypt_footer)
 {
-    uint64_t dev_sz;
     int rc = 0;
     int status;
 
-    rc = get_dev_sz(fs_blkdev, &dev_sz);
-    if (rc) {
-        return rc;
+    if (!dev_sz) {
+        int rc = get_dev_sz(fs_blkdev, &dev_sz);
+        if (rc) {
+            return rc;
+        }
     }
 
     /* Format the partition using the calculated length */
     if (crypt_footer) {
-        dev_sz -= CRYPT_FOOTER_OFFSET;
+        if (footer < 0) {
+            dev_sz += footer
+        } else {
+            dev_sz -= CRYPT_FOOTER_OFFSET;
+        }
     }
 
     std::string size_str = std::to_string(dev_sz / 4096);
@@ -97,7 +102,7 @@ static int format_ext4(char *fs_blkdev, char *fs_mnt_point, bool crypt_footer)
     return rc;
 }
 
-static int format_f2fs(char *fs_blkdev, uint64_t dev_sz, bool crypt_footer)
+static int format_f2fs(char *fs_blkdev, uint64_t dev_sz, uint64_t footer, bool crypt_footer)
 {
     int status;
 
@@ -110,7 +115,11 @@ static int format_f2fs(char *fs_blkdev, uint64_t dev_sz, bool crypt_footer)
 
     /* Format the partition using the calculated length */
     if (crypt_footer) {
-        dev_sz -= CRYPT_FOOTER_OFFSET;
+        if (footer < 0) {
+            dev_sz += footer
+        } else {
+            dev_sz -= CRYPT_FOOTER_OFFSET;
+        }
     }
 
     std::string size_str = std::to_string(dev_sz / 4096);
@@ -131,9 +140,9 @@ int fs_mgr_do_format(struct fstab_rec *fstab, bool crypt_footer)
            << " as '" << fstab->fs_type << "'";
 
     if (!strncmp(fstab->fs_type, "f2fs", 4)) {
-        rc = format_f2fs(fstab->blk_device, fstab->length, crypt_footer);
+        rc = format_f2fs(fstab->blk_device, fstab->length, fstab->length, crypt_footer);
     } else if (!strncmp(fstab->fs_type, "ext4", 4)) {
-        rc = format_ext4(fstab->blk_device, fstab->mount_point, crypt_footer);
+        rc = format_ext4(fstab->blk_device, fstab->mount_point, fstab->length, fstab->length, crypt_footer);
     } else {
         LERROR << "File system type '" << fstab->fs_type << "' is not supported";
     }
